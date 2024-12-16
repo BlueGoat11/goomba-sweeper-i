daea2970fc7e45e0e09c4a4c23def9dd641d4990
Luke Evanson <156948079+BlueGoat11@users.noreply.github.com>
2024-12-16 13:46:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
+function Turn_Right () {
+    hummingbird.setRotationServo(FourPort.Two, 100)
+    basic.pause(2900)
+    hummingbird.setRotationServo(FourPort.Two, 0)
+}
+function Forward_MARCH () {
+    hummingbird.setRotationServo(FourPort.One, -68)
+    hummingbird.setRotationServo(FourPort.Two, 50)
+}
+input.onButtonPressed(Button.A, function () {
+    hummingbird.setRotationServo(FourPort.One, 0)
+    hummingbird.setRotationServo(FourPort.Two, 0)
+})
+function Turn_Left () {
+    hummingbird.setRotationServo(FourPort.One, -100)
+    basic.pause(2900)
+    hummingbird.setRotationServo(FourPort.One, 0)
+}
+input.onButtonPressed(Button.B, function () {
+    hummingbird.setRotationServo(FourPort.One, 0)
+    hummingbird.setRotationServo(FourPort.Two, 0)
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     activate = 1
 })
 let activate = 0
-hummingbird.startHummingbird()
 basic.showLeds(`
     . . . . .
     # # # # #
@@ -10,11 +31,21 @@ basic.showLeds(`
     # # # # #
     . . . . .
     `)
+hummingbird.startHummingbird()
+hummingbird.setRotationServo(FourPort.One, 0)
+hummingbird.setRotationServo(FourPort.Two, 0)
 while (activate == 0) {
+    music.setVolume(40)
     music.play(music.stringPlayable("C5 A B G A F G E ", 120), music.PlaybackMode.UntilDone)
 }
 basic.showString("Cleaning")
 while (true) {
-    basic.showString("" + (randint(0, 10)))
-    basic.pause(100)
+    Turn_Left()
+    while (hummingbird.getSensor(SensorType.Distance, ThreePort.One) > 30) {
+        Forward_MARCH()
+    }
+    hummingbird.setRotationServo(FourPort.One, 0)
+    hummingbird.setRotationServo(FourPort.Two, 0)
+    Turn_Right()
+    break;
 }
